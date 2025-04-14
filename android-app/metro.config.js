9571827fe6c01925430bd7341b1b98bd07b2273e
longdb <longduongbao29@gmail.com>
2025-04-14 14:13:39 +0000

    [checkout] web branch

--- android-app/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-module.exports = (async () => {
-    const config = await getDefaultConfig(__dirname);
-    // Thêm 'cjs' vào danh sách các extension được resolve
-    config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
-    return config;
-})();
