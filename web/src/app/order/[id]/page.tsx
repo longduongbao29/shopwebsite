5267395ab7de2ddfc0e3c83ada97785f050ac2b3
longdb <longduongbao29@gmail.com>
2025-06-06 03:20:08 +0000

    [clean]

--- web/src/app/order/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import OrderPage from "@/src/app/order/page";
-type Params = Promise<{ id: string }>;
-export default async function OrderPageWrapper({ params }: { params:  Params  }) {
-  const {id} = await params
-  return <OrderPage id={id} />;
-}
